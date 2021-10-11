074455e97fe6aff1a3a1094cf93817399c09bfd6
Scott Hood <scottdothood@gmail.com>
2021-10-11 20:04:56 +0100

    Covering some easy exceptions
    Making sure a video element can be found before attempting to change
    anything.
    Adding a minimum/maximum limit to the playback rate.

--- background.js
+++ background.js
@@ -1,21 +1,30 @@
-
 function rateChange(command) {
+  /* Don't do anything if no video is found */
+  if (!document.querySelector('video')) return;
+  
+  const rateIncrement = 0.5;
+  const rateMax = 4;
+  const rateMin = rateIncrement;
   let rateChange = 0;
   
   switch (command) {
     case 'run-pbrs-increment':
-      rateChange = 0.5;
+      rateChange = rateIncrement;
       break;
     case 'run-pbrs-decrement':
-      rateChange = -0.5;
+      rateChange = -rateIncrement;
       break;
     case 'run-pbrs-reset':
       document.querySelector('video').playbackRate = 1;
+    default:
       return;
   }
   
-  /* Do not allow rate to drop below 0.5 */
-  if (rateChange >= 0 || document.querySelector('video').playbackRate !== 0.5)
+  const rateCurrent = document.querySelector('video').playbackRate;
+  
+  /* Do not allow rate to drop below the minimum or go above the maximum values */
+  if ((rateChange < 0 && rateCurrent !== rateMin) ||
+      (rateChange > 0 && rateCurrent !== rateMax))
     document.querySelector('video').playbackRate += rateChange;
 }
 
