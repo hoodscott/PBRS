5a9f1285e5bdf773b5173f811d388c0fe65c6b30
Scott Hood <scottdothood@gmail.com>
2021-10-25 17:49:03 +0100

    Updating script to accomodate multiple videos on the same page

--- background.js
+++ background.js
@@ -2,30 +2,39 @@ function rateChange(command) {
   /* Don't do anything if no video is found */
   if (!document.querySelector('video')) return;
   
+  /* Hardcode minimum/maximum/incremental values */
   const rateIncrement = 0.5;
   const rateMax = 4;
   const rateMin = rateIncrement;
-  let rateChange = 0;
   
+  /* Get the current rate from the first video on the page */
+  const rateCurrent = document.querySelector('video').playbackRate;
+  let rateNew = rateCurrent;
+  
+  /* Calculate the new rate based on input command */
   switch (command) {
     case 'run-pbrs-increment':
-      rateChange = rateIncrement;
+      rateNew += rateIncrement;
       break;
     case 'run-pbrs-decrement':
-      rateChange = -rateIncrement;
+      rateNew -= rateIncrement;
       break;
     case 'run-pbrs-reset':
-      document.querySelector('video').playbackRate = 1;
+      rateNew = 1;
+      break;
+    /* if no matching command is found, we can leave early */
     default:
       return;
   }
   
-  const rateCurrent = document.querySelector('video').playbackRate;
   
   /* Do not allow rate to drop below the minimum or go above the maximum values */
-  if ((rateChange < 0 && rateCurrent !== rateMin) ||
-      (rateChange > 0 && rateCurrent !== rateMax))
-    document.querySelector('video').playbackRate += rateChange;
+  if ((rateNew < 0 && rateCurrent !== rateMin) ||
+      (rateNew > 0 && rateCurrent !== rateMax))
+    /* Update all videos on the page with the new rate */
+    Array.from(document.querySelectorAll('video')).forEach(
+      el => el.playbackRate = rateNew
+    );
 }
 
 chrome.commands.onCommand.addListener((command, tab) => {
